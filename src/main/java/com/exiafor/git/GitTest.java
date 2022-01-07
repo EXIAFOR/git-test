62de3b75827c1b45538d9f01ff13c08219fd3338
exiafor <exiafor@qq.com>
2022-01-07 16:14:01 +0800

    second commit

--- src/main/java/com/exiafor/git/GitTest.java
+++ src/main/java/com/exiafor/git/GitTest.java
@@ -6,6 +6,7 @@ package com.exiafor.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!1");
+        System.out.println("Hello,Git!2");
     }
 }

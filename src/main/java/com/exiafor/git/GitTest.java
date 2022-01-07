325edb435b7067c0bd885f9d313b03d6381114eb
exiafor <exiafor@qq.com>
2022-01-07 16:12:31 +0800

    first commit

---
+++ src/main/java/com/exiafor/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.exiafor.git;
+
+/**
+ * @author exiafor
+ * @create 2022-01-07-16:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
